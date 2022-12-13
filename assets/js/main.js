5f1d2d5489d5c99b255e850f87f879b6aba399fa
Matheus <matheusteodoro04102004@gmail.com>
2022-12-13 18:16:06 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,13 @@ const offset = 0
 const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}limit=${limit}'
 
-fetch (url).then(function (response) {
-    response.json().then(function (responseBody) {
-        console.log(responseBody)
+fetch (url)
+    .then((response) => response.json ())
+    .then((jsonBody) => (
+        
+        console.log(jsonBody)
+    ))   
 
-    })
-}) 
 
-.catch(function (error) {
-    console.error(error)
-})
-.finally(function () {
-    console.finally('Requisição concluída!')       
-})
 
+    .catch(error => console.error(error))
